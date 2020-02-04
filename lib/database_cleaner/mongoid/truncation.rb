c2d003bd25137d5d4673f51c0d64d336048e588a
Micah Geisel <micah@botandrose.com>
2020-02-04 10:21:13 -0800

    drop support for mongoid 2 and below.

--- lib/database_cleaner/mongoid/truncation.rb
+++ lib/database_cleaner/mongoid/truncation.rb
@@ -1,6 +1,5 @@
 require 'database_cleaner/mongoid/base'
 require 'database_cleaner/generic/truncation'
-require 'database_cleaner/mongoid/mongo1_truncation_mixin'
 require 'database_cleaner/mongoid/mongo2_truncation_mixin'
 require 'database_cleaner/mongoid/mongoid_truncation_mixin'
 require 'mongoid/version'
@@ -11,17 +10,7 @@ module DatabaseCleaner
       include ::DatabaseCleaner::Mongoid::Base
       include ::DatabaseCleaner::Generic::Truncation
 
-      if ::Mongoid::VERSION < '3'
-
-        include ::DatabaseCleaner::Mongoid::Mongo1TruncationMixin
-
-        private
-
-        def database
-          ::Mongoid.database
-        end
-
-      elsif ::Mongoid::VERSION < '5'
+      if ::Mongoid::VERSION < '5'
 
         include ::DatabaseCleaner::Mongoid::MongoidTruncationMixin
 
