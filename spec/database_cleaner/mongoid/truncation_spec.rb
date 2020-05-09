60cf5a8820cf307f31b43a7d795b4acb31eeec8e
Micah Geisel <micah@botandrose.com>
2020-05-09 11:21:06 -0700

    remove pending specs that describe neither current behavior nor desired behavior.

--- spec/database_cleaner/mongoid/truncation_spec.rb
+++ spec/database_cleaner/mongoid/truncation_spec.rb
@@ -36,36 +36,6 @@ RSpec.describe DatabaseCleaner::Mongoid::Truncation do
         expect { described_class.new(foo: 'bar') }.to raise_error(ArgumentError)
       end
     end
-
-    describe "with pre_count optimization option" do
-      subject(:strategy) { described_class.new(pre_count: true) }
-
-      xit "only truncates non-empty tables" do
-        User.create!
-        expect(connection).to receive(:truncate_tables).with(tables)
-        strategy.clean
-      end
-    end
-
-    context 'when :cache_tables is set to true' do
-      xit 'caches the list of tables to be truncated' do
-        expect(connection).to receive(:database_cleaner_table_cache).and_return([])
-        expect(connection).not_to receive(:tables)
-
-        allow(connection).to receive(:truncate_tables)
-        described_class.new(cache_tables: true).clean
-      end
-    end
-
-    context 'when :cache_tables is set to false' do
-      xit 'does not cache the list of tables to be truncated' do
-        expect(connection).not_to receive(:database_cleaner_table_cache)
-        expect(connection).to receive(:database_tables).and_return([])
-
-        allow(connection).to receive(:truncate_tables)
-        described_class.new(cache_tables: false).clean
-      end
-    end
   end
 end
 
